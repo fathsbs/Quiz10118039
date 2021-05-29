440d1d4464d0ec06a6fd0bd641500acf6a4f60ae
fathsbs <fatahsbs@gmail.com>
2021-05-29 12:19:07 +0700

    Initial commit

---
+++ app/src/main/java/com/fathsbs/quiz10118039/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+Tanggal Pengerjaan      : Sabtu, 29 Mei 2021
+NIM                     : 10118039
+Nama                    : Fatahillah Satria Bima Seno
+Kelas                   : AKB-1
+
+ */
+
+package com.fathsbs.quiz10118039;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
